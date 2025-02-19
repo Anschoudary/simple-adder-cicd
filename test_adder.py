708fb95a8ff9eeb25c296a0cf578f261f51af4e3
anschoudry <ansmurtaza01@gmail.com>
2025-02-20 00:30:20 +0500

    Initial commit: Simple adder program with tests

---
+++ test_adder.py
@@ -0,0 +1,14 @@
+import pytest
+from adder import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-1, -4) == -5
+
+def test_add_mixed_numbers():
+    assert add(5, -2) == 3
+
+def test_add_zero():
+    assert add(0, 7) == 7
